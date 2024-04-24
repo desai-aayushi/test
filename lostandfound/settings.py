2c760571289ee67f93c915af9214346d70b95e1b
Desai Aayushi <46705941+desai-aayushi@users.noreply.github.com>
2024-04-24 01:13:47 -0400

    Update settings.py

--- lostandfound/settings.py
+++ lostandfound/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-wui%5vc%a0^a+^n44s41^a#)3yal1k=%wfu8%clyb9)fmuw*!p"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['test-rjnn', '.vercel.app']
 
 
 # Application definition
@@ -117,7 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
